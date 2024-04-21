6d8cefe89f122b9a2de60bc69339535af38bfac6
frisedel <frisedel@gmail.com>
2024-04-21 11:21:34 +0200

    add eslint html plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
     parser: "@typescript-eslint/parser",
-    plugins: ["react-hooks", "@typescript-eslint", "import", "simple-import-sort"],
+    plugins: ["react-hooks", "@typescript-eslint", "import", "simple-import-sort", "html"],
     ignorePatterns: ["/**/*.cjs"],
     parserOptions: {
         ecmaFeatures: {
